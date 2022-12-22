e08b085fc7c624ce081ce8bdbaa48aa712036938
Sidoryak Ostap <ostapsidorak@gmail.com>
2022-12-22 19:01:25 +0200

    control point 2

--- controlpoint2/src/App.js
+++ controlpoint2/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Footer from './Components/Footer';
+import Approach from './Components/OurApproach';
+import Header from './Components/Header';
+import ContactUs from './Components/ContactUs';
+import OurWorks from './Components/OurWorks';
+import Pricing from './Components/Pricing';
+import Projects from './Components/Projects';
+import Services from './Components/Services';
+import Testimonials from './Components/Testimonials';
+import Intro from './Components/Intro';
+import Video from './Components/Video';
+import Counters from './Components/Counters';
+import { useEffect, useRef, useState } from 'react';
+
+
 function App() {
+
+  const [IntroRefLink, SetIntroRefLink] = useState();
+
+  const IntroRef = useRef();
+  console.log(IntroRef);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header/>
+      <Intro Setref = {IntroRef}/>
+      <OurWorks />
+      <Counters />
+      <Projects />
+      <Approach />
+      <Testimonials/>
+      <Services/>
+      <Pricing />
+      <Video />
+      <ContactUs/>
+      <Footer />
     </div>
   );
 }
